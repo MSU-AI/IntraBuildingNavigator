39dfa8ae62e8abb07abf3b1ee0ea6a7b0137d3ea
Rachel Townson <townsonr@msu.edu>
2022-10-30 22:52:09 -0400

    added incomplete EB test

--- floor_plan.py
+++ floor_plan.py
@@ -1,52 +1,85 @@
+from enum import Enum, auto
 from typing import List
+
+class Node:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return str(self.name)
+
+# types of nodes based on: https://www.researchgate.net/publication/277318124_Generation_of_navigation_graphs_for_indoor_space 
+class Room(Node):
+    pass
+class Portal(Node):
+    pass
+class Stairwell(Node):
+    pass
+class Elevator(Node):
+    pass 
+
+class Edge:
+    # connection between two nodes (bidirectional)
+    def __init__(self, start_node, end_node, distance: int = 1):
+        if not isinstance(start_node, Node) or not isinstance(end_node, Node):
+            raise TypeError('Node type expected')
+
+        self.start_node = start_node
+        self.end_node = end_node
+        self.distance = distance
+
+    def __str__(self):
+        return "{} <-> {}".format(self.start_node, self.end_node)
 
 
 class FloorPlan:
     # floor plan is a dictionary of Nodes to all of their connected Edges
-    def __init__(self):
+    def __init__(self, name: str):
+        self.name = name
         self._graph = {}
 
     def __str__(self):
         output = ""
-
-    class Node:
-        def __init__(self, name: str):
-            self.name = name
-
-        def __str__(self):
-            return str(self.name)
-
-        # types of nodes based on: https://www.researchgate.net/publication/277318124_Generation_of_navigation_graphs_for_indoor_space 
-        class Room:
-            pass
-        class Portal:
-            pass
-        class Stairwell:
-            pass
-        class Elevator:
-            pass 
-
-    class Edge:
-        # connection between two nodes (bidirectional)
-        def __init__(self, start_node, end_node, path_cost: int = 1):
-            if not isinstance(start_node, FloorPlan.Node) or not isinstance(end_node, FloorPlan.Node):
-                raise TypeError('Node type expected')
-
-            self.start_node = start_node
-            self.end_node = end_node
-            self.path_cost = path_cost
-
-        def __str__(self):
-            return "{} -> {}".format(self.start_node, self.end_node)
+        for node, edges in self._graph.items():
+            output += ("\n" + str(node) + " : ")
+            for edge in edges:
+                output += ("\n" + "\t" + str(edge))
+        return output
 
     def add_node(self, node: Node, edges: List[Edge] = []):
         self._graph[node] = edges
 
     def add_edges_to_node(self, node: Node, edges: List[Edge]):
-        self._graph[node].append(edges)
+        for edge in edges:
+            if edge not in self._graph[node]:
+                self._graph[node].append(edge)
+    
+    def add_edges_by_node_name(self, node_name, edges):
+        for node in self._graph:
+            if node.name == node_name:
+                for edge in edges:
+                    self._graph[node].append(edge)
 
-    def add_edge(self, edge: Edge):
-        start_node = edge.start_node
-        end_node = edge.end_node
-        self.add_edges_to_node(start_node, [edge])
-        self.add_edges_to_node(start_node, [edge])
+    def add_edge(self, room: Edge):
+        start_node = room.start_node
+        end_node = room.end_node
+        self.add_edges_to_node(start_node, [room])
+        self.add_edges_to_node(start_node, [room])
+    
+    def add_room(self, room_name: str):
+        self.add_node(Room(room_name))
+
+    def add_consecutive_rooms(self, start_number: int, end_number: int):
+        for room_number in range(start_number, end_number+1):
+            self.add_room(Node("Room {}".format(room_number)))
+
+    def get_node_by_name(self, node_name):
+        for node in self._graph:
+            if node.name == node_name:
+                return node
+
+
+    
+
+
+    
--- test_floor_plan.py
+++ test_floor_plan.py
@@ -1,22 +1,142 @@
-from floor_plan import FloorPlan
+from floor_plan import FloorPlan, Node, Edge, Room, Portal
 
 
 def main():
-    test_floor_plan = FloorPlan()
+    eb = engineering_building_first_floor()
+    print(eb)
 
-    room_1 = FloorPlan.Node("room 1")
-    room_2 = FloorPlan.Node("room 2")
+    test = test_floor_plan()
+    print(test)
 
-    edge_1_to_2 = FloorPlan.Edge(room_1, room_2)
+def test_floor_plan() -> FloorPlan:
+    test_floor_plan = FloorPlan("test")
 
-    test_floor_plan.add_node(room_1)
-    test_floor_plan.add_node(room_2)
+    test_floor_plan.add_node("Room 1100")
+    test_floor_plan.add_room("Room 1103")
+    test_floor_plan.add_room("Room 1105")
+    test_floor_plan.add_room("Room 1107")
 
-    test_floor_plan.add_edge(edge_1_to_2)
-    print()
-    print(test_floor_plan._graph)
-    print()
+    test_floor_plan.add_edges_by_node_name(
+        "Room 1100", 
+        [
+            test_floor_plan.get_node_by_name("Room 1100"), 
+            test_floor_plan.get_node_by_name("Room 1103")
+        ]
+    )
 
+    return test_floor_plan
+
+def engineering_building_first_floor() -> FloorPlan:
+    eb_floor_1 = FloorPlan("Engineering Building")
+
+    # 11xx rooms
+    eb_floor_1.add_room("Room 1100")
+    eb_floor_1.add_room("Room 1103")
+    eb_floor_1.add_room("Room 1105")
+    eb_floor_1.add_room("Room 1107")
+    eb_floor_1.add_room("Room 1108")
+    eb_floor_1.add_room("Room 1109")
+    eb_floor_1.add_room("Room 1120")
+    eb_floor_1.add_room("Room 1118")
+    eb_floor_1.add_room("Room 1124")
+    eb_floor_1.add_room("Room 1126")
+    eb_floor_1.add_room("Room 1128")
+    eb_floor_1.add_room("Room 1129")
+    eb_floor_1.add_room("Room 1133")
+    eb_floor_1.add_room("Room 1135")
+    eb_floor_1.add_room("Room 1138")
+    eb_floor_1.add_room("Room 1140")
+    eb_floor_1.add_room("Room 1130")
+    eb_floor_1.add_room("Room 1145")
+    eb_floor_1.add_room("Room 1148")
+    eb_floor_1.add_room("Room 1160")
+
+    # 12xx rooms
+    eb_floor_1.add_room("Room 1200")
+    eb_floor_1.add_room("Room 1202")
+    eb_floor_1.add_room("Room 1204")
+    eb_floor_1.add_room("Room 1206")
+    eb_floor_1.add_room("Room 1208")
+    eb_floor_1.add_room("Room 1210")
+    eb_floor_1.add_room("Room 1211")
+    eb_floor_1.add_room("Room 1213")
+    eb_floor_1.add_room("Room 1215")
+    eb_floor_1.add_room("Room 1216")
+    eb_floor_1.add_room("Room 1217")
+    eb_floor_1.add_room("Room 1218")
+    eb_floor_1.add_room("Room 1219")
+    eb_floor_1.add_room("Room 1220")
+    eb_floor_1.add_room("Room 1225")
+    eb_floor_1.add_room("Room 1226")
+    eb_floor_1.add_room("Room 1228")
+    eb_floor_1.add_room("Room 1232")
+    eb_floor_1.add_room("Room 1230")
+    eb_floor_1.add_room("Room 1233")
+    eb_floor_1.add_room("Room 1234")
+    eb_floor_1.add_room("Room 1237")
+    eb_floor_1.add_room("Room 1239")
+    eb_floor_1.add_room("Room 1240")
+    eb_floor_1.add_room("Room 1243")
+    eb_floor_1.add_room("Room 1245")
+    eb_floor_1.add_room("Room 1248")
+    eb_floor_1.add_room("Room 1252")
+    eb_floor_1.add_room("Room 1254")
+    eb_floor_1.add_room("Room 1255")
+    eb_floor_1.add_room("Room 1257")
+    eb_floor_1.add_room("Room 1258")
+    eb_floor_1.add_room("Room 1259")
+    eb_floor_1.add_room("Room 1261")
+    eb_floor_1.add_room("Room 1262")
+    eb_floor_1.add_room("Room 1260")
+    eb_floor_1.add_room("Room 1270")
+    eb_floor_1.add_room("Room 1271")
+    eb_floor_1.add_room("Room 1273")
+    eb_floor_1.add_room("Room 1274")
+
+    # 13xx rooms
+    eb_floor_1.add_room("Room 1300")
+    eb_floor_1.add_room("Room 1303")
+    eb_floor_1.add_room("Room 1306")
+    eb_floor_1.add_room("Room 1307")
+    eb_floor_1.add_room("Room 1312")
+    eb_floor_1.add_room("Room 1318")
+    eb_floor_1.add_room("Room 1320")
+    eb_floor_1.add_room("Room 1328")
+    eb_floor_1.add_room("Room 1325")
+    eb_floor_1.add_room("Room 1342")
+    eb_floor_1.add_room("Room 1338")
+    eb_floor_1.add_room("Room 1340")
+    eb_floor_1.add_room("Room 1345")
+
+    # 14xx rooms
+    eb_floor_1.add_room("Room 1408")
+    eb_floor_1.add_room("Room 1410")
+    eb_floor_1.add_room("Room 1415")
+    eb_floor_1.add_room("Room 1416")
+    eb_floor_1.add_room("Room 1420")
+    eb_floor_1.add_room("Room 1422")
+    eb_floor_1.add_room("Room 1424")
+    eb_floor_1.add_room("Room 1426")
+    eb_floor_1.add_room("Room 1428")
+
+    # 15xx rooms
+    eb_floor_1.add_room("Room 1500")
+    eb_floor_1.add_room("Room 1501")
+    eb_floor_1.add_room("Room 1502")
+    eb_floor_1.add_room("Room 1503")
+    eb_floor_1.add_consecutive_rooms(1508, 1516)
+    eb_floor_1.add_room("Room 1520")
+    eb_floor_1.add_room("Room 1522")
+    eb_floor_1.add_room("Room 1526")
+    eb_floor_1.add_room("Room 1528")
+    eb_floor_1.add_room("Room 1530")
+    eb_floor_1.add_room("Room 1531")
+    eb_floor_1.add_room("Room 1532")
+    eb_floor_1.add_room("Room 1535")
+    eb_floor_1.add_room("Room 1538")
+    eb_floor_1.add_room("Room 1540")
+
+    return eb_floor_1
 
 if __name__ == '__main__':
     main()
